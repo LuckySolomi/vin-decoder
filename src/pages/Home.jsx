f706b2fa857a4082a1a8ce7d0ebac965faaad40c
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2026-02-17 02:17:00 +0200

    Creates Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,53 @@
+import { useState } from "react";
+import { decodeVin } from "../services/api";
+import VinForm from "../components/VinForm";
+import VinResults from "../components/VinResults";
+
 function Home() {
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleDecode = async (vin) => {
+    try {
+      setError("");
+      setLoading(true);
+
+      const data = await decodeVin(vin);
+
+      const excludedVariables = [
+        "Error Code",
+        "Error Text",
+        "Suggested VIN",
+        "Possible Values",
+        "Additional Error Text",
+      ];
+
+      const filtered = data.Results.filter(
+        (item) =>
+          item.Value &&
+          item.Value !== "Not Applicable" &&
+          !excludedVariables.includes(item.Variable),
+      );
+      setResults(filtered);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <>
-      <h1>Welcome to the Home</h1>
-    </>
+    <section>
+      <h1>VIN Decoder</h1>
+
+      <VinForm onDecode={handleDecode} />
+
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <VinResults results={results} />
+    </section>
   );
 }
 
