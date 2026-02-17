5039ebbc3d34bb2181bd30d727191631c4d7f9fd
Solomiia Pitsur <pitsur.solomiia@gmail.com>
2026-02-17 21:04:11 +0200

    Adds link to the details in VariableList

--- src/components/variableList/VariableList.jsx
+++ src/components/variableList/VariableList.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styles from "./VariableList.module.css";
 
 function VariableList({ variables }) {
@@ -5,7 +6,12 @@ function VariableList({ variables }) {
     <div className={styles.wrapper}>
       {variables.map((item) => (
         <div key={item.ID} className={styles.card}>
-          <h3 className={styles.title}>{item.Name}</h3>
+          <h3>
+            <NavLink to={`/variables/${item.ID}`} className={styles.titleLink}>
+              {item.Name}
+            </NavLink>
+          </h3>
+
           <p className={styles.description}>
             {item.Description?.replace(/<[^>]+>/g, "")}
           </p>
